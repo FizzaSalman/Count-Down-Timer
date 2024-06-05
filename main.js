0522793e9f5009c298a6cf821994fc4f405e5b5d
FizzaSalman <159646126+FizzaSalman@users.noreply.github.com>
2024-06-05 16:39:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+#! /usr/bin/env/node
+import { differenceInSeconds } from "date-fns";
+import inquirer from "inquirer";
+const res = await inquirer.prompt({
+    type: "number",
+    name: "userInput",
+    message: "Please Enter the amount of second",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return ("Please Enter the valid number");
+        }
+        else if (input > 60) {
+            return ("Seconds must be in 60");
+        }
+        else {
+            return true;
+        }
+    }
+});
+//console.log(val); for checking 
+let input = res.userInput;
+function startTime(val) {
+    const inTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(inTime);
+    setInterval(() => {
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+        if (timeDiff <= 0) {
+            console.log("Time Up");
+            process.exit();
+        }
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")} :${sec.toString().padStart(2, "0")}`);
+    }, 1000);
+}
+startTime(input);
+//const intTime = new Date().setSeconds(new Date().getSeconds() + 1)
+//const intervalTime = new Date(intTime)
+//const date = new Date(intTime)  just for checking
+//console.log(date)
+//console.log("intTime" + intervalTime)
